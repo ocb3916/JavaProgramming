76e65ed83848643e18c019fb8469f8f926ca2795
ocb3916 <dhchanbin1004@gmail.com>
2023-09-25 12:56:02 +0900

    4주차 과제

---
+++ chap01/src/Week4/Q10.java
@@ -0,0 +1,27 @@
+package Week4;
+
+import java.util.Scanner;
+
+public class Q10 {
+    public static void main(String[] args) {
+        System.out.print("양의 정수를 입력하세요 : ");
+        int num = new Scanner(System.in).nextInt();
+        if (isPrime(num))
+            System.out.println(num + "는 소수입니다.");
+        else
+            System.out.println(num + "는 소수가 아닙니다.");
+    }
+    public static boolean isPrime(int num) {
+        boolean isPrime;
+        if (num == 1) 
+            isPrime = false;
+        else {
+            for (int i = 2; i <= num; i++) {
+                if (num % i == 0)
+                    isPrime = true;
+                    break;
+            }
+        }
+        return isPrime;
+    }
+}
---
+++ chap01/src/Week4/Q4.java
@@ -0,0 +1,15 @@
+package Week4;
+
+// 문제 4
+// 다음 실행 결과를 출력하는 프로그램을 for 문을 사용해 작성하라.
+
+public class Q4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
