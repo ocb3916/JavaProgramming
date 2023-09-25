00b5eaf20710d9b82e6b22279f24c8254fa8aa0b
ocb3916 <dhchanbin1004@gmail.com>
2023-09-25 17:24:02 +0900

    4주차 과제

--- chap01/src/Week4/Q10.java
+++ chap01/src/Week4/Q10.java
@@ -1,5 +1,8 @@
 package Week4;
 
+// 문제 10
+// 다음은 주어진 정수가 소수(prime)인지를 조사하는 프로그램의 일부이다. isPrime() 메서드를 완성하라. 여기서 소수는 1보다 크면서 1과 자신 외에는 나누지 않는 수이다.
+
 import java.util.Scanner;
 
 public class Q10 {
@@ -11,17 +14,12 @@ public class Q10 {
         else
             System.out.println(num + "는 소수가 아닙니다.");
     }
-    public static boolean isPrime(int num) {
-        boolean isPrime;
-        if (num == 1) 
-            isPrime = false;
-        else {
-            for (int i = 2; i <= num; i++) {
-                if (num % i == 0)
-                    isPrime = true;
-                    break;
-            }
+    
+    static boolean isPrime(int num) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0)
+                return false;
         }
-        return isPrime;
+        return true;
     }
 }
--- chap01/src/Week4/Q4.java
+++ chap01/src/Week4/Q4.java
@@ -6,9 +6,8 @@ package Week4;
 public class Q4 {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < i + 1; j++) {
+            for (int j = 0; j < i + 1; j++)
                 System.out.print("*");
-            }
             System.out.println();
         }
     }
